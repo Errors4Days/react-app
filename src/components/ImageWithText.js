0d89bf756db441fe1d12d32f04808a42e6f9de58
error4days <justin.j.lee14@gmail.com>
2021-07-11 17:48:21 -0400

    Added background on the images to increase text visibility.

--- src/components/ImageWithText.js
+++ src/components/ImageWithText.js
@@ -1,24 +1,31 @@
 import React, {Component} from 'react';
 import {AiFillGithub, AiFillLinkedin, AiOutlineMail} from "react-icons/ai";
-
+///style={{backgroundImage: 'url("' + this.props.image + '")'}}
 class ImageWithText extends Component {
   render() {
     return(
-      <section className="image" style={{backgroundImage: 'url("' + this.props.image + '")'}}>
-        <div className="container">
-          <div className="img-title">{this.props.title}</div>
-          <div className="img-subtitle">{this.props.subtitle}</div>
-          {this.props.icons && <div className="icons">
-            <a href="mailto:justinleecontact62@gmail.com">
-              <AiOutlineMail className="icon" size={"80px"}/>
-            </a>
-            <a href="https://linkedin.com/in/justin-lee-272692174">
-              <AiFillLinkedin className="icon" size={"80px"}/>
-            </a>
-            <a href="https://github.com/Errors4Days">
-              <AiFillGithub className="icon" size={"80px"}/>
-            </a>
-          </div>}
+      <section>
+        <div className="image" style={{backgroundImage: 'url("' + this.props.image + '")'}}>
+          <div className="img-container">
+            <div className="image-textbox">
+
+            <div className="img-title">{this.props.title}</div>
+            <div className="img-subtitle">{this.props.subtitle}</div>
+
+            {this.props.icons && <div className="icons">
+              <a href="mailto:justinleecontact62@gmail.com">
+                <AiOutlineMail className="icon" size={"80px"}/>
+              </a>
+              <a href="https://linkedin.com/in/justin-lee-272692174">
+                <AiFillLinkedin className="icon" size={"80px"}/>
+              </a>
+              <a href="https://github.com/Errors4Days">
+                <AiFillGithub className="icon" size={"80px"}/>
+              </a>
+            </div>}
+
+            </div>
+          </div>
         </div>
       </section>
     )
--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import PageHeader from '../PageHeader';
+import {AiFillGithub, AiFillLinkedin, AiOutlineMail} from "react-icons/ai";
 
 class Contact extends Component {
   componentDidMount() {
@@ -14,12 +15,24 @@ class Contact extends Component {
             title="Contact Me!"
             subtitle="Use this google form to contact me. You can also contact me via Linkedin or email."
           />
+          
+          <div id="contact-icons">
+            <a href="mailto:justinleecontact62@gmail.com">
+              <AiOutlineMail className="icon" size={"80px"}/>
+            </a>
+            <a href="https://linkedin.com/in/justin-lee-272692174">
+              <AiFillLinkedin className="icon" size={"80px"}/>
+            </a>
+            <a href="https://github.com/Errors4Days">
+              <AiFillGithub className="icon" size={"80px"}/>
+            </a>
+          </div>
 
-            <iframe 
+          <iframe 
             src="https://docs.google.com/forms/d/e/1FAIpQLScPzH6hJ02a9-Q-ObHgc6H79oGPBA1iHmPszFhYNkJLn7pVTw/viewform?embedded=true" 
             id="contact-form" title="contact" width="620" height="900" frameBorder="0">
               Loading…
-            </iframe>          
+          </iframe>          
         </div>
       </div>
     );
