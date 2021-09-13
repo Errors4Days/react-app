ddbf3d435633a664691720ce70c6605104b459e4
Error4Days <jxl1850@case.edu>
2021-09-13 10:05:36 -0400

    Added Blender work

--- src/components/pages/Blog.js
+++ src/components/pages/Blog.js
@@ -2,34 +2,11 @@ import React, {Component} from 'react';
 import ImageWithText from '../ImageWithText';
 import image from '../assets/img/header-blog.jpg';
 import ToDoList from '../todo/ToDoList';
-import * as THREE from 'three';
+//import * as THREE from 'three';
 
 class Blog extends Component {
   componentDidMount() {
-    document.title = "My Blog"
-    const scene = new THREE.Scene();
-		const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-
-    const renderer = new THREE.WebGLRenderer();
-    renderer.setSize( window.innerWidth, window.innerHeight);
-    document.body.appendChild( renderer.domElement );
-
-    const geometry = new THREE.BoxGeometry();
-    const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-    const cube = new THREE.Mesh( geometry, material );
-    scene.add( cube );
-
-    camera.position.z = 5;
-
-    const animate = function () {
-      requestAnimationFrame( animate );
-
-      cube.rotation.x += 0.01;
-      cube.rotation.y += 0.01;
-
-      renderer.render( scene, camera );
-		};
-		animate();
+    document.title = "My Blog";
   }
 
   render() {
@@ -43,7 +20,7 @@ class Blog extends Component {
         />
         <section className="textbox1">
           <div className="container">
-            <h2>Interesting React components</h2>
+            <h2>Interesting React Components</h2>
             <p>
               This page is dedicated to exploring interesting React applications. So far it contains a to-do list and an animation
               experiment.
@@ -60,7 +37,47 @@ class Blog extends Component {
         </section>
 
         <section className="textbox1">
-          <div ref={ref => (this.mount = ref)}/>
+          <div className="container">
+            <h2>Fun with Blender</h2>
+            <p>
+              Over the summer I learned React and some blender. I wanted to learn blender after seeing really impressive 3D animations.
+              Blender is a 3D rendering tool for creating models and animations. Here is some of my work!
+            </p>
+
+            <div>
+              <h3>Fancy Rolling Ball</h3>
+              <iframe width="600" height="500" src="https://www.youtube.com/embed/17szWB6p2M4" 
+              title="YouTube video player" frameborder="0" 
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; " allowfullscreen></iframe>
+              <p>
+                This is my most recent render. I took inspiration from a youtube challenge, in which contestants would animate a
+                a falling ball. I used a 3D rendering service, Blendergrid to render out the video since it would have taken many hours to 
+                render on my own computer.
+              </p>
+            </div>
+
+            <div>
+              <h3>Basic Rolling Ball</h3>
+              <iframe width="600" height="500" src="https://www.youtube.com/embed/kHPvQkOv_Og" title="YouTube video player" 
+              frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media;" allowfullscreen></iframe>            
+            </div>
+
+            <div>
+              <h3>Chair Tutorial</h3>
+              <iframe width="600" height="500" src="https://www.youtube.com/embed/4ZrlCMsR9bw" title="YouTube video player" 
+              frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media;" allowfullscreen></iframe>
+            </div>
+
+            <div>
+              <h3>Doughnut Tutorial</h3>
+              <iframe width="600" height="500" src="https://www.youtube.com/embed/IYf4LonneIE" 
+              title="YouTube video player" frameborder="0" 
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media;" allowfullscreen></iframe>
+              <p>
+                I followed a Blender guru's guide.
+              </p>
+            </div>
+          </div>
         </section>
       </div>
     );
